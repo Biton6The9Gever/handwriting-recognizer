cdbbb5b633a717e0cd6a0ab386894e34247f6ff5
Biton6The9Gever <yehonatanbiton4@gmail.com>
2025-09-09 14:55:11 +0300

    added first version of data augmenter script

---
+++ augment_images.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageOps
+
+# path for the images not including the index and extension
+IMAGE_ROOT = "../ML_Project/SentencePictures/Sentence_"
+SENTENCE_AMOUNT=48
+
+def inverse_colors(image_path,i):
+    """Invert the colors of the given image."""
+    if(image_path==None):
+        return
+    image=Image.open(image_path)
+    image=ImageOps.invert(image)
+    image.save(IMAGE_ROOT+"{:03d}.jpg".format(i+SENTENCE_AMOUNT))
+
+
+
+for i in range(1,SENTENCE_AMOUNT+1):
+    image_path=IMAGE_ROOT+"{:03d}.jpg".format(i)
+    inverse_colors(image_path,i)
+# Open the image
+img = Image.open("../ML_Project/SentencePictures/Sentence_001.jpg")
+
+
+
