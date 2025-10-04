494946141ae83fe41b154c07b4def6e7bae4315f
Biton6The9Gever <yehonatanbiton4@gmail.com>
2025-10-05 01:10:14 +0300

    deleted old data set

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from Scripts.utils import Utils
 
-# Utils.create_dataset()
-# Utils.get_images_size_distribution(Utils.DATA_PATH)
-# Utils.recreate_data_folder()
-Utils.create_csv_file()
+Utils.create_dataset()
+Utils.get_images_size_distribution(Utils.DATA_PATH)
+Utils.recreate_data_folder()
+#Utils.create_csv_file()
