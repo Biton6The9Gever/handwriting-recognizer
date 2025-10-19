4c8546bab008658b9a7bb9ca2e85b3fd6a71108d
Biton6The9Gever <yehonatanbiton4@gmail.com>
2025-10-20 02:12:51 +0300

    removed temporary file

--- src/data_creation/temp.py
+++
@@ -1,7 +0,0 @@
-import os
-
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
-DATA_DIR = os.path.join(PROJECT_ROOT, "data")
-RAW_DATA_PATH = os.path.join(DATA_DIR, "raw")
-PROCESSED_DATA_PATH = os.path.join(DATA_DIR, "processed")
-DATA_CSV = os.path.join(DATA_DIR, "dataset.csv")
