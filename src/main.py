df6e016b18aa807877b63adf5e2b50d4b06d47b8
Biton6The9Gever <yehonatanbiton4@gmail.com>
2025-12-19 22:21:27 +0200

    Refactor image processing pipeline: recreate data folder, generate synthetic sentence images, and update model prediction logic
    - Removed the model prediction loop from main.py and added a call to recreate_data_folder().
    - Introduced a new temporary script (temp.py) to generate synthetic sentence images and corresponding JSON labels.
    - Added three sample images and labels for "HELLO WORLD", "MACHINE LEARNING", and "HANDWRITING OCR".
    - Updated the image generation logic to handle letter pasting and canvas creation.

--- src/main.py
+++ src/main.py
@@ -1,21 +1,10 @@
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-from utils import load_highest_accuracy_model,predict_image
+from utils import load_highest_accuracy_model,predict_image ,create_dataset ,recreate_data_folder
 from models.rnn_model import build_model_rnn
 
-model=load_highest_accuracy_model()
+recreate_data_folder()
 
-folder_path='D:/Biton/VisualStudio/.VSC/Python/ML_Project/data/samples'
-
-for filename in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, filename)
-    
-    if os.path.isfile(file_path):
-        try:
-            result = predict_image(model, file_path)
-            print(f"{filename}: {result}")
-        except Exception as e:
-            print(f"[WARN] Error with {filename}: {e}")
 
 
 
